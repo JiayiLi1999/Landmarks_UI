293f1a94c66980aefaf12462070e7d6a9c12fe86
andyJiayiLi <lijiayi606@gmail.com>
2022-07-03 22:49:08 -0700

    Handling User Input

--- Landmarks/Views/Landmarks/LandmarkList.swift
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @EnvironmentObject var modeldata:ModelData
     @State private var showFavoritesOnly = true
     var filteredLandmarks:[Landmark]{
-        landmarks.filter{landmark in
+        modeldata.landmarks.filter{landmark in
              (!showFavoritesOnly||landmark.isFavorite)
         }
     }
@@ -37,7 +38,7 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)","iPhone 11","iPad (9th generation)"],id: \.self){ deviceName in
+        ForEach(["iPhone 11"],id: \.self){ deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
